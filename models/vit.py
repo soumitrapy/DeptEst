202eedcbfcdc20c3fb45c5200f4c719b533e1c92
Soumitra <soumitrapy@gmail.com>
2025-04-29 18:11:16 +0530

    vit 1

--- models/vit.py
+++ models/vit.py
@@ -1,6 +1,8 @@
 from torch.nn import TransformerEncoderLayer
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
+
 class VIT(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -8,10 +10,44 @@ class VIT(nn.Module):
         self.encs = nn.Sequential(*[TransformerEncoderLayer(d_model=config['model']['d_model'], nhead=config['model']['nhead'], batch_first=True) for _ in range(config['model']['n_encoder_layers'])])
     
     def forward(self, x):
-        x = x.squeeze(1)
+        x = self.im2v(x)
         enc = self.encs(x)
-        return enc.unsqueeze(1)
+        return self.v2im(enc)
 
+    def im2v(self, x): # (B, 1, 256, 256)
+        t = F.unfold(x, kernel_size=self.config['model']['patch_size'], stride=self.config['model']['patch_size']) # shape: (B, C * 16 * 16, N)
+        return t.transpose(-2, -1)
+    def v2im(self, p):
+        p = p.transpose(-2,-1)
+        return F.fold(p, output_size = (self.config['dataset']['img_size'], self.config['dataset']['img_size']), kernel_size=self.config['model']['patch_size'], stride=self.config['model']['patch_size'])
+
+
+class VITInput:
+    def __init__(self, image_size=256, patch_size = 16):
+        self.image_size = image_size
+        self.patch_size = patch_size
+    def im2v(self, x): # (B, 1, 256, 256)
+        t = F.unfold(x, kernel_size=self.patch_size, stride=self.patch_size) # shape: (B, C * 16 * 16, N)
+        return t.transpose(-2, -1)
+    def v2im(self, p):
+        p = p.transpose(-2,-1)
+        return F.fold(p, output_size = (self.image_size, self.image_size), kernel_size=self.patch_size, stride=self.patch_size)
+    
+
+# # Assume you already have these:
+# images = torch.randn(10, 1, 256, 256)
+# patches = F.unfold(images, kernel_size=16, stride=16)  # shape: (10, 256, 256)
+
+# # Now reconstruct:
+# reconstructed = F.fold(
+#     patches,                     # shape: (B, C*16*16, N)
+#     output_size=(256, 256),      # original image size
+#     kernel_size=16,
+#     stride=16
+# )
+
+# print(reconstructed.shape)  # (10, 1, 256, 256)
+# torch.allclose(images, reconstructed)  # Should be True
 if __name__=="__main__":
     x = torch.randn(10, 1, 256, 256)
     config = {'model': {'n_encoder_layers': 1,
